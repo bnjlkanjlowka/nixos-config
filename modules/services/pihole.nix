d2a9c4a54ea8f7e24616212bcb85adac6bc117ff
bnjlka <dolwans12@gmail.com>
2026-09-03 15:01:46 +0300

    fix pihole

--- modules/services/pihole.nix
+++ modules/services/pihole.nix
@@ -4,6 +4,20 @@
   services = {
     pihole-ftl = {
       enable = true;
+      settings = {
+        dns = {
+          upstreams = [ "192.168.30.1#53" ];
+        };
+      };
+
+      lists = [
+        {
+          url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.plus.txt";
+          type = "block";
+          enabled = true;
+          description = "hagezi blocklist pro plus";
+        }
+      ];
     };
     pihole-web = {
       enable = true;
