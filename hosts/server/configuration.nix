9da4fccb8d6ddb796b1210288cc0a8ee0285700d
bnjlka <dolwans12@gmail.com>
2026-09-03 19:33:01 +0300

    fix for nginx

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -12,7 +12,7 @@
     ../../modules/services/pihole.nix
     ../../modules/services/unbound.nix
     ../../modules/services/navidrome.nix
-    ../../modules/service/nginx.nix
+    ../../modules/services/nginx.nix
 
     ../../modules/system
     ../../modules/system/users.nix
