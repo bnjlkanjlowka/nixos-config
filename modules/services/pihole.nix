4692b5699ffae564bd20c3cf7198712116c3ac51
bnjlka <dolwans12@gmail.com>
2026-09-03 14:11:41 +0300

    fix pihole

--- modules/services/pihole.nix
+++ modules/services/pihole.nix
@@ -4,6 +4,9 @@
   services = {
     pihole-ftl = {
       enable = true;
+      settings = {
+        dns.interface = [ "192.168.30.112" ];
+      };
     };
     pihole-web = {
       enable = true;
