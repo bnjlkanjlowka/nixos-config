032ff19cc0d47635c6d2147f08bbe02cadf25fff
bnjlka <dolwans12@gmail.com>
2026-09-04 20:02:20 +0300

    add transmission module

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -7,16 +7,8 @@
       rpc-bind-address = "0.0.0.0";
       download-dir = "/data";
     };
-  };
 
-  networking.firewall = {
-    allowedTCPPorts = [
-      9091
-      51413
-    ];
-    allowedUDPPorts = [
-      9091
-      51413
-    ];
+    openRPCPort = true;
+    openPeerPorts = true;
   };
 }
