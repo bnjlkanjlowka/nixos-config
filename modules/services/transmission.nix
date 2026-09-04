a47971d05cbab37260f8647d39bbf70b93bb8f28
bnjlka <dolwans12@gmail.com>
2026-09-04 20:17:18 +0300

    add transmission module

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -6,6 +6,7 @@
     settings = {
       rpc-bind-address = "0.0.0.0";
       rpc-whitelist = "127.0.0.1,192.168.*.*,10.0.10.*";
+      rpc-host-whitelist = "*.spb.lan,*.home.lan";
       download-dir = "/data";
     };
 
