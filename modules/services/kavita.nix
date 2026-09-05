5e606877c49b0e59c0e19b38bcb226387a3c97a4
bnjlka <dolwans12@gmail.com>
2026-09-05 17:27:07 +0300

    add kavita token

--- modules/services/kavita.nix
+++ modules/services/kavita.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ config, ... }:
 
 {
+  sops = {
+    secrets.kavita-token = {
+      sopsFile = ../../secrets/kavita/token;
+      format = "binary";
+    };
+  };
+
   services.kavita = {
     enable = true;
+    tokenKeyFile = config.sops.secrets.kavita-token;
   };
 
   networking.firewall = {
