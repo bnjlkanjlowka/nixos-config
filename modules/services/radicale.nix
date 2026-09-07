22aa7864731d2f4797f05607630510971fd36fce
bnjlka <dolwans12@gmail.com>
2026-09-07 14:27:48 +0300

    add radicale module

---
+++ modules/services/radicale.nix
@@ -0,0 +1,39 @@
+{ ... }:
+
+{
+  services.radicale = {
+    enable = true;
+    settings = {
+      auth = {
+        type = "htpasswd";
+        htpasswd_filename = "/etc/radicale/users";
+        htpasswd_encryption = "bcrypt";
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "dolwans12@gmail.com";
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."contacts.bnjlkanjlowka.xyz" = {
+      enableACME = true;
+      forceSSL = true;
+
+      locations."/" = {
+        proxyPass = "http://localhost:5232";
+        extraConfig = ''
+          proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header  X-Forwarded-Host $host;
+          proxy_set_header  X-Forwarded-Port $server_port;
+          proxy_set_header  X-Forwarded-Proto $scheme;
+          proxy_set_header  Host $host;
+          proxy_pass_header Authorization;
+        '';
+      };
+    };
+  };
+}
